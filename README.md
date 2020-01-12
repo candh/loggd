8bb34c8621f529eed1ee8e02e513aaed0c24156a
candh <khichihaider@gmail.com>
2020-01-12 21:10:29 +0500

    minor version update

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # [loggd](https://candh.gitbooks.io/loggd)
+
 [![Travis](https://img.shields.io/travis/candh/loggd.svg)](https://travis-ci.org/candh/loggd)
 [![npm](https://img.shields.io/npm/dm/loggd.svg)](https://www.npmjs.com/package/loggd)
 [![npm](https://img.shields.io/npm/v/loggd.svg)](https://www.npmjs.com/package/loggd)
@@ -16,18 +17,14 @@ Listen, you just want to log something? or might just want to save and retrieve 
       "nestedObjects?": {
         "You": "Got It!"
       },
-      "arrays?": [
-        "No",
-        "Problem"
-      ]
+      "arrays?": ["No", "Problem"]
     },
     "_id": "49c6f6ee-5805-4499-95e0-53bfc0bdc7ad"
   }
 ]
 ```
 
-
-### Installation 
+### Installation
 
 ```bash
 $ npm install loggd --save
@@ -36,8 +33,8 @@ $ npm install loggd --save
 <br>
 
 For API documentation, detailed explanation and examples:
-# [Read The Docs](https://candh.gitbooks.io/loggd)
 
+# [Read The Docs](https://candh.gitbooks.io/loggd)
 
 ### Tests
 
@@ -46,23 +43,32 @@ $ npm test
 ```
 
 ### Contribution
+
 See [`CONTRIBUTING.md`](https://github.com/candh/loggd/blob/master/CONTRIBUTING.md)
 
 ### Changelog
+
 Added in version 2.0.0
 
+**v2.2.0**
+
+- Only adds an \_id field if not already specified. Be careful, now its up to you to make them unique if you did in fact supply your own.
+- Ran prettier on the whole project
+
 **v2.1.0**
-* 🎉 new method: clear() - Clears the database file
-* Replaced stupid docstrings with jsdocs. should display a little bit of help on your fancy editors now. 
-* Better tests. Really. (i think, [i mean you can never be THAT sure..](https://pbs.twimg.com/media/Ci9dn7vWYAAGbuV.jpg))
-* Updated dependencies
-* Moved db dir to test
-* Refactoring
-* Loggd is a class now. (Nothing for you to worry about)
+
+- 🎉 new method: clear() - Clears the database file
+- Replaced stupid docstrings with jsdocs. should display a little bit of help on your fancy editors now.
+- Better tests. Really. (i think, [i mean you can never be THAT sure..](https://pbs.twimg.com/media/Ci9dn7vWYAAGbuV.jpg))
+- Updated dependencies
+- Moved db dir to test
+- Refactoring
+- Loggd is a class now. (Nothing for you to worry about)
 
 **v2.0.0**
-* **MAJOR API CHANGES**, If your project is using a previous version and you update this package, **everything will break!** I'm really sorry! But this is the cleanest version yet. Please [read the docs](https://candh.gitbooks.io/loggd/). If you liked the previous version, You'll ❤️ this one!
 
+- **MAJOR API CHANGES**, If your project is using a previous version and you update this package, **everything will break!** I'm really sorry! But this is the cleanest version yet. Please [read the docs](https://candh.gitbooks.io/loggd/). If you liked the previous version, You'll ❤️ this one!
 
 ### Wait, who made it?
+
 By [@candhforlife](http://twitter.com/candhforlife) mostly seen at COMSATS Lahore doing CS stuff to get a CS degree
--- test/test.js
+++ test/test.js
@@ -1,194 +1,223 @@
 // --------------------------------------
-const loggd = require("../lib/loggd")
-const assert = require("assert")
-const db = new loggd('./db/test.json');
+const loggd = require("../lib/loggd");
+const assert = require("assert");
+const db = new loggd("./db/test.json");
 // --------------------------------------
 
 // --------------------------------------
-const data = { username: "Vito Corleone", type: "Human" }
+const data = { username: "Vito Corleone", type: "Human" };
 const data2 = [
-    { username: "Tom Hagen", type: "Human", profession: "Lawyer" },
-    { username: "Micheal Corleone", type: "Human" },
-    { username: "Kay", type: "Human" },
-    { username: "Fredo", type: "Human" },
-    { username: "Kanye", type: "Human", profession: "Artist", albums: [] },
-    { username: "Jay Z", type: "Human", profession: "Artist", albums: ["4:44"] },
-    { username: "Frank", type: "Animal", skills: ["loving", "loyalty"], legs: 4 },
-    { username: "Stanley", type: "Animal", skills: ["hating", "no idea what he's doing"], legs: 4 },
-]
+  { username: "Tom Hagen", type: "Human", profession: "Lawyer" },
+  { username: "Micheal Corleone", type: "Human" },
+  { username: "Kay", type: "Human" },
+  { username: "Fredo", type: "Human" },
+  { username: "Kanye", type: "Human", profession: "Artist", albums: [] },
+  { username: "Jay Z", type: "Human", profession: "Artist", albums: ["4:44"] },
+  { username: "Frank", type: "Animal", skills: ["loving", "loyalty"], legs: 4 },
+  {
+    username: "Stanley",
+    type: "Animal",
+    skills: ["hating", "no idea what he's doing"],
+    legs: 4
+  }
+];
+const data3 = { _id: "uniqueId", name: "Smith" };
 
 const sData = { type: "Animal" };
 const sData2 = { username: "Kanye" };
 const sData3 = { username: "Fredo" };
 // --------------------------------------
 
-describe('Loggd Testing\n------------------', function() {
-    //
-    // loggd.clear()
-    //
-    describe("db.clear()", () => {
-        it('should clear the file and then check if we get an empty array', (done) => {
-            db.clear()
-                .then(() => {
-                    db.read()
-                        .then((data) => {
-                            assert.deepStrictEqual(data, [])
-                            done()
-                        })
-                        .catch((err) => done(err))
-                })
-                .catch((err) => done(err))
-        })
-    })
-
-    // 
-    // loggd.insert(data)
-    // 
-    describe('db.insert()', function() {
-        it('should save 1 object in the file', function(done) {
-            db.insert(data)
-                .then((d) => {
-                    assert.deepStrictEqual(data, d[0])
-                    done()
-                })
-                .catch((err) => done(err))
-        });
-    });
-    
-    describe('db.insert([])', function() {
-        it('should save an array of objects in the file', function(done) {
-            db.insert(data2)
-                .then((data) => {
-                    assert.deepStrictEqual(data2, data)
-                    done()
-                })
-                .catch((err) => done(err))
-        });
-    });
-
-    // 
-    // loggd.read()
-    // 
-    describe('db.read()', function() {
-        it('should read the file', function(done) {
-            db.read()
-                .then((data) => {
-                    assert.ok(data.length === [data, ...data2].length)
-                    done()
-                })
-                .catch((err) => done(err))
-        });
-    });
-
-    // 
-    // loggd.find(criteria)
-    // 
-    describe('db.find()', function() {
-        it('should find all the type: Animal objects', function(done) {
-            db.find(sData)
-                .then((Data) => {
-                    assert.ok(Data.result.length === 2)
-                    done()
-                })
-                .catch((err) => done(err))
-        });
-    });
-
-    // 
-    // loggd.updateOne()
-    // 
-    describe('updateOne()', function() {
-        it('should update one object', function(done) {
-            db.find(sData2)
-                .then((data) => {
-                    let username = 'Kanye West'
-                    let albums = ['The College Dropout', 'Late Registration', 'Graduation']
-                    data.result[0].username = username
-                    for (const album of albums) {
-                       data.result[0].albums.push(album) 
-                    }
-                    data.updateOne(0).then((data) => {
-                        data = data[0]
-                        assert.ok(data.username === username)
-                        assert.deepStrictEqual(data.albums, albums)
-                        done()
-                    }).catch((err) => done(err))
-                })
-                .catch((err) => done(err))
-        });
-    });
-
-    // 
-    // loggd.updateAll()
-    // 
-    describe('updateAll()', function() {
-        it('should update all the objects that it found', (done) => {
-            db.find(sData)
-                .then((data) => {
-                    const skill = "Just being amazing"
-                    data.result.forEach((e) => {
-                        e.cute = true
-                        e.skills.push(skill)
-                    })
-                    data.updateAll().then((d) => {
-                        for (const o of d) {
-                           assert.ok(o.cute === true) 
-                           assert.ok(o.skills.indexOf(skill) !== -1)
-                        }
-                        done()
-                    })
-                })
-                .catch((err) => done(err))
-        })
-    })
-
-    // 
-    // loggd.removeOne()
-    // 
-    describe('removeOne()', function() {
-        it('should delete one object', function(done) {
-            db.find(sData3)
-                .then((data) => {
-                    data.removeOne(0).then((d) => {
-                        db.find(sData3)
-                            .then((Data) => {
-                                assert.deepStrictEqual(Data.result, [])
-                                done()
-                            })
-                    })
-                    .catch((err) => done(err))
-                })
-                .catch((err) => done(err))
-        })
-    })
-
-    // 
-    // loggd.removeAll()
-    // 
-    describe('removeAll()', () => {
-        it('should remove all objects of type Animal', (done) => {
-            db.find(sData)
-                .then((Data) => {
-                    Data.removeAll()
-                        .then(() => {
-                            db.find(sData)
-                                .then((Data) => {
-                                    // >muh javascirpt is so sexy
-                                    // >language of the future
-                                    // foh
-                                    assert.deepStrictEqual(Data.result, [])
-                                    done()
-                                })
-                                .catch((err) => done(err))
-                        }).catch((err) => done(err))
-                }).catch((err) => done(err))
-        })
-    })
-
-    after((done) => {
-        db.clear()
-            .then(() => {
-                done()
+describe("Loggd Testing\n------------------", function() {
+  //
+  // loggd.clear()
+  //
+  describe("db.clear()", () => {
+    it("should clear the file and then check if we get an empty array", done => {
+      db.clear()
+        .then(() => {
+          db.read()
+            .then(data => {
+              assert.deepStrictEqual(data, []);
+              done();
             })
-    })
-})
+            .catch(err => done(err));
+        })
+        .catch(err => done(err));
+    });
+  });
+
+  //
+  // loggd.insert(data)
+  //
+  describe("db.insert()", function() {
+    it("should save 1 object in the file", function(done) {
+      db.insert(data)
+        .then(d => {
+          assert.deepStrictEqual(data, d[0]);
+          done();
+        })
+        .catch(err => done(err));
+    });
+  });
+
+  //
+  // loggd.insert(data)
+  //
+  describe("db.insert()", function() {
+    it("should save 1 object in the file without modifiying its _id", function(done) {
+      db.insert(data3)
+        .then(d => {
+          assert.deepStrictEqual(data3, d[0]);
+          done();
+        })
+        .catch(err => done(err));
+    });
+  });
+
+  describe("db.insert([])", function() {
+    it("should save an array of objects in the file", function(done) {
+      db.insert(data2)
+        .then(data => {
+          assert.deepStrictEqual(data2, data);
+          done();
+        })
+        .catch(err => done(err));
+    });
+  });
+
+  //
+  // loggd.read()
+  //
+  describe("db.read()", function() {
+    it("should read the file", function(done) {
+      db.read()
+        .then(data => {
+          assert.ok(data.length === [data, data3, ...data2].length);
+          done();
+        })
+        .catch(err => done(err));
+    });
+  });
+
+  //
+  // loggd.find(criteria)
+  //
+  describe("db.find()", function() {
+    it("should find all the type: Animal objects", function(done) {
+      db.find(sData)
+        .then(Data => {
+          assert.ok(Data.result.length === 2);
+          done();
+        })
+        .catch(err => done(err));
+    });
+  });
+
+  //
+  // loggd.updateOne()
+  //
+  describe("updateOne()", function() {
+    it("should update one object", function(done) {
+      db.find(sData2)
+        .then(data => {
+          let username = "Kanye West";
+          let albums = [
+            "The College Dropout",
+            "Late Registration",
+            "Graduation"
+          ];
+          data.result[0].username = username;
+          for (const album of albums) {
+            data.result[0].albums.push(album);
+          }
+          data
+            .updateOne(0)
+            .then(data => {
+              data = data[0];
+              assert.ok(data.username === username);
+              assert.deepStrictEqual(data.albums, albums);
+              done();
+            })
+            .catch(err => done(err));
+        })
+        .catch(err => done(err));
+    });
+  });
+
+  //
+  // loggd.updateAll()
+  //
+  describe("updateAll()", function() {
+    it("should update all the objects that it found", done => {
+      db.find(sData)
+        .then(data => {
+          const skill = "Just being amazing";
+          data.result.forEach(e => {
+            e.cute = true;
+            e.skills.push(skill);
+          });
+          data.updateAll().then(d => {
+            for (const o of d) {
+              assert.ok(o.cute === true);
+              assert.ok(o.skills.indexOf(skill) !== -1);
+            }
+            done();
+          });
+        })
+        .catch(err => done(err));
+    });
+  });
+
+  //
+  // loggd.removeOne()
+  //
+  describe("removeOne()", function() {
+    it("should delete one object", function(done) {
+      db.find(sData3)
+        .then(data => {
+          data
+            .removeOne(0)
+            .then(d => {
+              db.find(sData3).then(Data => {
+                assert.deepStrictEqual(Data.result, []);
+                done();
+              });
+            })
+            .catch(err => done(err));
+        })
+        .catch(err => done(err));
+    });
+  });
+
+  //
+  // loggd.removeAll()
+  //
+  describe("removeAll()", () => {
+    it("should remove all objects of type Animal", done => {
+      db.find(sData)
+        .then(Data => {
+          Data.removeAll()
+            .then(() => {
+              db.find(sData)
+                .then(Data => {
+                  // >muh javascirpt is so sexy
+                  // >language of the future
+                  // foh
+                  assert.deepStrictEqual(Data.result, []);
+                  done();
+                })
+                .catch(err => done(err));
+            })
+            .catch(err => done(err));
+        })
+        .catch(err => done(err));
+    });
+  });
+
+  after(done => {
+    db.clear().then(() => {
+      done();
+    });
+  });
+});
